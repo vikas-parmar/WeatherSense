e797f9b8bca0e35e03dddee4022e2889e53d9017
Vikas Panwar <vkparmar003@gmail.com>
2023-03-19 20:38:49 +0530

    Search icon created &   UI improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import githubLogo from "./assets/githubLogo.svg";
-
+import search from "./assets/search-outline.svg"
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=319de7333d6f77f7ea98d69dbc188698`;
+
   const searchLocation = (e) => {
     if (e.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=319de7333d6f77f7ea98d69dbc188698`)
+      fetch(URL)
         .then(res => res.json())
         .then(data => setData(data));
       setLocation('');
     }
+  }
+
+  const handleClick = () => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => setData(data));
+    setLocation('');
   }
   return (
     <div className="app">
@@ -23,13 +32,14 @@ function App() {
           onKeyDown={searchLocation}
           placeholder='Enter Location'
         />
+        <img src={search} className="search--button" onClick={handleClick} />
       </div>
 
       <div className="container">
-        <h1 className="location">{data.name}</h1>
+        <h1 className="location">{data.name || data.message ? data.name || data.message : 'Weather-Card'}</h1>
         <div className="description">
           {data.main ? <h1 className="temp">{data.main.temp.toFixed()}°F</h1> : null}
-          {data.weather ? <p>{data.weather[0].main}</p> : null}
+          {data.weather ? <p>{data.weather[0].main}</p> : <p>Try Another City</p>}
         </div>
 
         {data.name !== undefined &&
@@ -47,11 +57,11 @@ function App() {
               <p>Wind Speed</p>
             </div>
           </div>}
-          
+
       </div>
-          <div className="logo"><a href="https://github.com/viksa7111"><img src={githubLogo} alt="vikas-parmar-github" /></a></div>
+      <div className="logo"><a href="https://github.com/viksa7111"><img src={githubLogo} alt="vikas-parmar-github" /></a></div>
     </div>
   )
 }
 
-export default App
+export default App;
