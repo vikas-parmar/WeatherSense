29b7df894d420e9d00dfd1ebae8ae3a1e4a27797
Vikas Parmar <vkparmar003@gmail.com>
2023-06-28 10:31:02 +0530

    User Interface updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './style.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
